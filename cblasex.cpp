bbfb72e1b9e9e0a7a3cd5c0e6087e981575dc673
Alexander Crabbe <acrabbe@electron226.PHYSnet.uni-hamburg.de>
2017-09-22 15:58:40 +0200

    -

--- cblasex.cpp
+++ cblasex.cpp
@@ -4,107 +4,99 @@
 #include "cblas.h"
 #include<string>
 
-//function to print matrix values
+//create matrix class
 class mat {
 	public:
-   mat(double *matrix,int a, int b, std::string mname){double S=*matrix; int m=b; int k=b; std::string name=mname;};
+	double* S;
+	int row;
+	int col;
+	std::string name;
 	
-void pmatrix (void) {  
+    mat(double* matrix,int mrow, int mcol, std::string mname) {
+	   S=matrix; 
+	   row=mrow; 
+	   col=mcol; 
+	   name=mname;
+	   };
+	   
+//function to set matrix elements
+	void smatrix (void);
+//function print matrix elements
+	void pmatrix (void);
+};
+   	
+//implement class functions  
+void mat::smatrix (void) {  
+	int i=0;
+	int j=1;
+   for(i=0;i<row*col;i++){
+		     if(i=1) S[i]=1;
+		
+			   /*else {	
+				   S[i]=2;
+				}*/
+			};
+}; 
+	
+ void mat::pmatrix (void) {  
 	int i=0;
     int j=0;
     
     std::cout << name << std::endl; 
-   for(i=0;i<m;i++){
-	   for(j=0;j<k;j++){std::cout << S[i+(j*(k+1))] << ",";}
-	    std::cout << std::endl;
-	}
-	    std::cout << std::endl;
-   };	   
- };
- 
-int main(){
-	
+   
+   for(i=0;i<row;i++){
+	   for(j=0;j<col;j++){
+	   std::cout << S[i+(j*row)] << ",";
+			}
+	   std::cout << std::endl;
+		} 
+	std::cout << std::endl;
+	}; 
 
+//main function
+int main(){
     
 //size of matrices A,B,C
-
-	const int M=5; //Rows of A
-	const int N=1; //Columns B
+	const int M=3; //Rows of A
+	const int N=2; //Columns B
 	const int K=2; //Columns of A Rows of B
 
    
 //equation parameters
-
 	const double alpha=1;
 	const double beta=0;
 	const int lda=M;
 	const int ldb=K;
 	const int ldc=M;  
-   
- //define matrices
- 
-	double A[M*K]={6,7,3,2,2,2};
-    double B[N*K]={4,5};
+	
+  
+//declare matrices
+	double A[M*K];
+    double B[K*N];
 	double C[M*N];
-	
-	int j=0;
-	
-	for(j=0;j<M*K;j++){
-				A[j]=j;
-			};
-   
-	/*
-	double * AP = (double*) malloc(sizeof(double)*M*N);
-	
-	AP[0] = 3;
-	AP[1] = 2;
-	AP[2] = 1;
-	AP[2] = 2;
 
-	std::cout << sizeof(double) << std::endl;
-	
-	std::cout << AP << std::endl;
-	std::cout << *AP << std::endl;
-	std::cout << *(AP+1) << std::endl;
-	std::cout << *(AP+2) << std::endl;
-	std::cout << *(AP+3) << std::endl;
-	
-	std::cout << A[0] << std::endl;
-	std::cout << A[1] << std::endl;
-	std::cout << A[2] << std::endl;
-	std::cout << alpha << std::endl;
-	std::cout << &alpha << std::endl;
-	*/
+//create matrix objects
+mat mat1(A,M,K,"A");
+mat mat2(B,K,N,"B");
+mat mat3(C,M,N,"C");
 
-//dgemm function
+//set elements of A and B
+mat1.smatrix();
+mat2.smatrix();
 
+//call dgemm function to calculate AB
 	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
 				M, N, K, 
 				alpha, A,
 				lda, B, ldb,
 				beta,C,ldc);
-                   
- //print results
- 
- mat mat1(A,M,K,"A");
 
-	mat1.pmatrix;
-	/*
-		std::cout << "A=" << std::endl;
-		
-	
-		
-		std::cout << "B=" << std::endl;
-		
-		Pmatrix(B,K,N);
-		
-		std::cout << "C=" << std::endl;
-		
-		Pmatrix(C,M,N);
-		
-   */
+
+//print results
+mat1.pmatrix();
+mat2.pmatrix();
+mat3.pmatrix();
+
+std::cout << A[1] << A[2]<< std::endl;
 	return 0;          
 }
-			 
-			 
-			 
